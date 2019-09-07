fb160fd020d30b17cdf8c5deaea19d793f7c6ddb
BAlmeidaS <bruno.almeida.silveira@gmail.com>
2019-09-07 20:26:37 -0300

    adding a plot performance on monitor taxi v2

--- taxi-v2/main.py
+++ taxi-v2/main.py
@@ -3,6 +3,15 @@ from monitor import interact
 import gym
 import numpy as np
 
+import matplotlib.pyplot as plt
+
 env = gym.make('Taxi-v3')
 agent = Agent()
 avg_rewards, best_avg_reward = interact(env, agent)
+
+# plot performance
+plt.plot(np.linspace(0, 20000, len(avg_rewards), endpoint=False),
+         np.asarray(avg_rewards))
+plt.ylabel('Average Reward')
+plt.xlabel('Episode Number')
+plt.show()
