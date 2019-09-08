423cd3bce59e791d62b7547c0f0401f696cbda1a
BAlmeidaS <bruno.almeida.silveira@gmail.com>
2019-09-08 02:10:14 -0300

    creating a default logging

---
+++ taxi/default_logger.py
@@ -0,0 +1,13 @@
+import os
+import json
+import logging.config
+
+
+def setup_logging(log_name="debug", path="logging.json"):
+    if os.path.exists(path):
+        with open(path, "rt") as f:
+            config = json.load(f)
+        config["handlers"]["info_file_handler"]["filename"] = log_name + ".log"
+        logging.config.dictConfig(config)
+    else:
+        raise FileNotFoundError(f"{path} not found")
