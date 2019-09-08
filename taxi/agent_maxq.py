f0f993b20acb65a24deac060247b75a870c12cb1
BAlmeidaS <bruno.almeida.silveira@gmail.com>
2019-09-08 17:04:08 -0300

    adding a maxq algorith and a greedy search using ray to find best params

---
+++ taxi/agent_maxq.py
@@ -0,0 +1,126 @@
+import numpy as np
+from collections import defaultdict
+
+
+stack = []
+
+
+def epsilon_policy(q_s, e, nA):
+    if np.array_equal(q_s, np.zeros(nA)):
+        return np.ones(nA) * 1/nA
+
+    policy_s = np.ones(nA) * [e/nA]
+    best_action = np.argmax(q_s)
+    policy_s[best_action] = 1 - e + e/nA
+    return policy_s
+
+
+def epsilon_policy_new(q_s, e, nA):
+    if np.array_equal(q_s, np.zeros(nA)):
+        return np.ones(nA) * 1/nA
+
+    policy_s = np.zeros(nA)
+    best_action = np.argmax(q_s)
+    policy_s[best_action] = 1
+    return policy_s
+
+
+class Subtask:
+    def __init__(self):
+        self.C = defaultdict(lambda: np.zeros(len(self.nodes)))
+
+    def select_action(self, state, epsilon):
+        index = np.random.choice(len(self.nodes),
+                                 p=epsilon_policy(self.C[state],
+                                                  epsilon,
+                                                  len(self.nodes)))
+
+        stack.append((self, state, index))
+
+        subtask = self.nodes[index]
+
+        return subtask.select_action(state, epsilon)
+
+
+class AgentMaxQ(Subtask):
+    def __init__(self, alpha=0.9, gamma=1):
+        self.nodes = [Get(),
+                      Put()]
+
+        self.alpha = alpha
+        self.gamma = gamma
+
+        super().__init__()
+
+    def select_action(self, state, epsilon):
+        stack.clear()
+
+        return super().select_action(state, epsilon)
+
+    def step(self, state, action, reward, next_state, done, epsilon):
+
+        for node, state, action in stack:
+            future_policy = epsilon_policy(node.C[next_state], epsilon, len(node.nodes))
+
+            node.C[state][action] += self.alpha * (reward
+                                                   + (self.gamma
+                                                      * future_policy.dot(node.C[next_state]))
+                                                   - node.C[state][action])
+
+        stack.clear()
+
+
+class Get(Subtask):
+    def __init__(self):
+        super().__init__()
+
+        self.nodes = [Navigate(),
+                      Pickup()]
+
+
+class Put(Subtask):
+    def __init__(self):
+        super().__init__()
+
+        self.nodes = [Navigate(),
+                      Putdown()]
+
+
+class Navigate(Subtask):
+    def __init__(self):
+        super().__init__()
+
+        self.nodes = [South(),
+                      North(),
+                      East(),
+                      West()]
+
+
+class South:
+    def select_action(self, state, *args):
+        return 0
+
+
+class North:
+    def select_action(self, state, *args):
+        return 1
+
+
+class East:
+    def select_action(self, state, *args):
+        return 2
+
+
+class West:
+    def select_action(self, state, *args):
+        return 3
+
+
+class Pickup:
+    def select_action(self, state, *args):
+        return 4
+
+
+class Putdown:
+    def select_action(self, state, *args):
+        return 5
--- taxi/main.py
+++ taxi/main.py
@@ -1,4 +1,4 @@
-from agent_expected_sarsa import AgentExpectedSarsa
+from agent_maxq import AgentMaxQ
 from monitor import interact
 import gym
 import numpy as np
@@ -12,54 +12,79 @@ from tqdm import tqdm
 import logging
 from default_logger import setup_logging
 
+import ray
+
 
 def main():
     env = gym.make('Taxi-v3')
 
-    agent = AgentExpectedSarsa()
-    avg_rewards, best_avg_reward, plot_rewards = interact(env, agent)
+    agent = AgentMaxQ(alpha=0.1, gamma=1)
+    avg_rewards, best_avg_reward, plot_rewards = interact(env, agent, 20000, decay_epsilon=25)
 
     print(f"best_avg_reward: {best_avg_reward}")
 
     # plot performance
-    plt.plot(np.linspace(0, 200000, len(plot_rewards), endpoint=False),
+    plt.plot(np.linspace(0, 20000, len(plot_rewards), endpoint=False),
              np.asarray(plot_rewards))
     plt.ylabel('Average Reward')
     plt.xlabel('Episode Number')
     plt.show()
 
 
-def greedy_search():
-    setup_logging("expected_sarsa_gs")
+@ray.remote
+def greedy_search(x, total):
+    setup_logging("maxq_gs_" + str(x))
 
-    alphas = np.arange(.8, 1.03, 0.03)
-    gammas = np.arange(.8, 1.03, 0.03)
-    eps_decay = range(5, 110, 15)
+    alphas = np.array([.1, .2, .5, .9, .95, 1])
+    gammas = np.array([.9, .95, .99, 1])
+    eps_decay = [10, 30, 70, 100, 150]
 
     # linear product of each variable
-    vrs = list(product(alphas, gammas, eps_decay))
+    all_objs = list(product(alphas, gammas, eps_decay))
+
+    np.random.shuffle(all_objs)
+
+    fator = int(len(all_objs)/total)
+
+    if x == 0:
+        vrs = all_objs[:fator]
+    elif x == (total-1):
+        vrs = all_objs[x*fator:]
+    else:
+        vrs = all_objs[x*fator: (x+1)*fator]
 
     # Initializing result dictionary
     results = {}
 
     # looping for each
-    for a, g, e_decay in tqdm(vrs, desc='greedy search'):
+    for a, g, e_decay in tqdm(vrs, desc='greedy search', leave=False):
         results[(a, g, e_decay)] = run(a, g, e_decay)
 
-    # get the top 10 results
-    best_results = sorted(results.items(), key=lambda x: x[1], reverse=True)[:10]
+    # get the top 100 results
+    best_results = sorted(results.items(), key=lambda x: x[1], reverse=True)[:50]
 
     # log the best results
     for i in best_results:
         logging.info(f"alpha: {i[0][0]} gamma: {i[0][1]} decay: {i[0][2]} - avg_reward: {i[1]}")
 
+#    logging.info(best_results)
+
+
+def parallel():
+    ray.init()
+    try:
+        futures = [greedy_search.remote(i, 8) for i in range(8)]
+        ray.get(futures)
+    finally:
+        ray.shutdown()
+
 
 def run(alpha, gamma, decay_ep):
     env = gym.make('Taxi-v3')
-    agent = AgentExpectedSarsa(alpha=alpha, gamma=gamma)
+    agent = AgentMaxQ(alpha=alpha, gamma=gamma)
     avg_rewards, best_avg_reward, plot_rewards = interact(env,
                                                           agent,
-                                                          4000,
+                                                          20000,
                                                           decay_epsilon=decay_ep)
     return best_avg_reward
 
